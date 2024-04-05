99cc402b1d85ede401e1c08f83c7e285a41b22bc
Ramkumar Ramaiya <Ramkumar.Ramaiya@byteforza.com>
2024-04-05 16:06:20 +0530

    code committed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'; 
 import { HomeComponent } from './home/home.component';
-import { AppComponent } from './app.component'; 
 const routes: Routes = [
   { path: '', component: HomeComponent }, 
 ]
 
-@NgModule({ 
+@NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }

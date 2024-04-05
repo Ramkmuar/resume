afe10420adc537d5e2c84590aef0f47dcee6c08c
Ramkumar Ramaiya <Ramkumar.Ramaiya@byteforza.com>
2024-04-05 15:12:57 +0530

    connection changed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router'; 
-import { HomeComponent } from './home/home.component';
+import { RouterModule, Routes } from '@angular/router';  
+import { AppComponent } from './app.component';
 const routes: Routes = [
-  { path: '', component: HomeComponent }, 
+  { path: '', component: AppComponent }, 
 ]
 
 @NgModule({
